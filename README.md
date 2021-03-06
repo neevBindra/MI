a121597e239294bae8f63bb8cb4b3a4e402e0676
neevBindra <73393895+neevBindra@users.noreply.github.com>
2021-03-06 21:14:07 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# MI
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,19 @@
+
+var thief
+var 
+
+function preload(){
+
+}
+
+function setup(){
+createCanvas(800,800);
+thief = createSprite(100,200,20,20)
+}
+
+
+function draw(){
+  background("black");
+
+  drawSprites();
+}
